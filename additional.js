678a728acaf7e0524676edeb8d8081887b466687
Daniel Ji <Jidaniel1234@gmail.com>
2017-11-09 15:26:31 -0800

    Big Fat Update 11/9

--- additional.js
+++ additional.js
@@ -1,6 +1,9 @@
 // Window load event used just in case window height is dependant upon images
 $(document).ready(() => {
 
+  $('#vid1').hide();
+  $('#vid1').fadeIn(1500);
+
   $(window).on('scroll', function(){
     let win = $(window).scrollTop();
     if (win > 100) {
