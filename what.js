41f1b35f4e8b1b5fd5a30e4f2ba91b2b532fce95
Daniel Ji <Jidaniel1234@gmail.com>
2017-11-27 16:22:43 -0800

    Small Change 11/26

--- what.js
+++ what.js
@@ -8,7 +8,7 @@ $(document).ready(() => {
 
   $(window).on('scroll', function(){
     let win = $(window).scrollTop();
-    if (win > 100) {
+    if (win > 20) {
       $('.nav').addClass('onScroll1');
     } else {
       $('.nav').removeClass('onScroll1');
