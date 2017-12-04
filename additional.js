730eb32b80a72665baa3402827945a2e1dcf35a7
Daniel Ji <Jidaniel1234@gmail.com>
2017-12-03 16:59:30 -0800

    Final Edits! (I Hope)

--- additional.js
+++ additional.js
@@ -1,8 +1,7 @@
 // Window load event used just in case window height is dependant upon images
 $(document).ready(() => {
-  var vid = document.getElementById("vid1");
-  vid.autoplay = false;
-  vid.load();
+  $('#vid1').autoplay(false);
+  $('#vid1').load();
 
   $('#vid1').hide();
   $('.nav').hide();
